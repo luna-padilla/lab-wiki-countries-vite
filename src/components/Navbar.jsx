577ae1908f19a2fd5581295ea2ae5794c8477cb3
luna <lunapadilla.8@gmail.com>
2024-11-27 19:53:40 +0100

    feat:add links and flags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,15 @@
-function Navbar() {}
+function Navbar({ className }) {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            WikiCountries
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,54 @@
-function HomePage() {}
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function HomePage({ className }) {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then((response) => {
+        setCountries(response.data);
+      });
+    return () => {};
+  }, []);
+  return (
+    <div
+      className="container"
+      style={{ maxHeight: "90vh", overflow: "scroll" }}
+    >
+      <h1 style={{ fontSize: "24px" }}>
+        WikiCountries: Your Guide to the World
+      </h1>
+
+      <div className="list-group ">
+        {countries.map((country) => {
+          return (
+            <Link
+              className="list-group-item list-group-item-action"
+              key={`${country.alpha3Code}`}
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                className="img-fluid"
+                src={`https://flagcdn.com/16x12/${country.alpha2Code.toLowerCase()}.png`}
+                srcSet={`
+    https://flagcdn.com/32x24/${country.alpha2Code.toLowerCase()}.png 2x,
+    https://flagcdn.com/48x36/${country.alpha2Code.toLowerCase()}.png 3x
+  `}
+                width="16"
+                height="12"
+                alt={`${country.name.common} flag`}
+              />
+
+              {country.name.common}
+            </Link>
+          );
+        })}
+       
+      </div>
+    </div>
+  );
+}
 
 export default HomePage;
